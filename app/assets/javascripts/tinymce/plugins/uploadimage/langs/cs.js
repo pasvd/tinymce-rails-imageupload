39fabef1cf2eeab53dfddca64965cc201e646d2e
Pavel Svoboda <svoboda@insiaoffice.cz>
2015-08-13 21:20:33 +0200

    add yeti i18n

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/cs.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('cs', {
+  'Insert an image from your computer': 'Insert an image from your computer',
+  'Insert image': 'Insert image',
+  'Choose an image': "Vyberte obrázek",
+  'You must choose a file': "You must choose a file",
+  'Got a bad response from the server': "Got a bad response from the server",
+  "Didn't get a response from the server": "Didn't get a response from the server",
+  'Insert': "Vložit",
+  'Cancel': "Zrušit",
+  'Image description': "Image description",
+});
