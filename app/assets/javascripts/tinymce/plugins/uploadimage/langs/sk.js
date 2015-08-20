e011f2d559e7dcb049a32199e5630160e2294a9d
Pavel Svoboda <svoboda@insiaoffice.cz>
2015-08-20 14:32:29 +0200

    sk lozcalization

---
+++ app/assets/javascripts/tinymce/plugins/uploadimage/langs/sk.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('sk', {
+  'Insert an image from your computer': 'Vložte obrázek z vašeho počítače',
+  'Insert image': 'Vložte obrázek',
+  'Choose an image': "Vyberte obrázek",
+  'You must choose a file': "Musíte vybrat soubor",
+  'Got a bad response from the server': "Špatná odpověď ze serveru",
+  "Didn't get a response from the server": "Nelze získat odpověď serveru",
+  'Insert': "Vložit",
+  'Cancel': "Zrušit",
+  'Image description': "Popis obrázku"
+});
